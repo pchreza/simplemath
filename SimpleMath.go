29c056daa4f29487f265a3b648b3570bc5d0ec91
unknown <pchreza@gmail.com>
2024-06-08 02:16:20 +0330

    first try to make a golang module for test ! :)))

---
+++ SimpleMath.go
@@ -0,0 +1,76 @@
+package SimpleMath
+
+// Add returns the sum of two integers.
+//
+// Parameters:
+// - a: an integer
+// - b: an integer
+//
+// Returns:
+// - an integer representing the sum of a and b
+func Add(a, b int) int {
+	return a + b
+}
+
+// Sub subtracts two integers and returns the result.
+//
+// Parameters:
+// - a: the first integer
+// - b: the second integer
+//
+// Returns:
+// - the result of subtracting b from a
+func Sub(a, b int) int {
+	return a - b
+}
+
+// Mul multiplies two integers and returns the result.
+//
+// Parameters:
+// - a: an integer value
+// - b: an integer value
+//
+// Returns:
+// - the product of a and b
+func Mul(a, b int) int {
+	return a * b
+}
+
+// Div divides two integers and returns the result.
+//
+// Parameters:
+// - a: an integer
+// - b: an integer
+//
+// Returns:
+// - the quotient of a divided by b
+func Div(a, b int) int {
+	return a / b
+}
+
+// Mod returns the remainder of dividing a by b.
+//
+// Parameters:
+// - a: an integer value
+// - b: an integer value
+//
+// Returns:
+// - an integer value representing the remainder of a divided by b
+func Mod(a, b int) int {
+	return a % b
+}
+
+// Pow calculates the power of a number.
+//
+// It takes two integer parameters, a and b, where a is the base and b is the exponent.
+// The function returns the result of raising a to the power of b.
+//
+// Parameters:
+// - a: an integer representing the base.
+// - b: an integer representing the exponent.
+//
+// Returns:
+// - an integer representing the result of raising a to the power of b.
+func Pow(a, b int) int {
+	return a ^ b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pchreza/simplemath
+
+go 1.22.3
